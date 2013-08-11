63aa4327b5c075298327f4889158abfcb2073efc
Tor <tornilssonohrn@gmail.com>
2013-08-11 09:31:26 +0200

    fixed preview image cropping but not very nicely

--- assets/themes/torManifest/js/torManifest.js
+++ assets/themes/torManifest/js/torManifest.js
@@ -11,17 +11,49 @@ $(function () {
 	});
 
 	// SHOWING THE COVERPICTURE PREVIEW BELONING TO A POST
-	var background = $('#background');
+	// var background = [];
+	// background[0] = $('#backgroundLeft');
+	// background[1] = $('#backgroundRight');
 
-	$('.prevImg').each( function (i) {
+	// $('.prevImg').each( function (i) {
+	// 	var previewImage = new Image();
+	// 	previewImage.src = $(this).attr('src');
+	// 	previewImage.onload = function () {
+	// 		this.css({
+	// 			'background': 'url(' + previewImage.src + ')' + ' center center no-repeat',
+	// 			'position': 'absolute',
+	// 			'background-size': 'cover'
+	// 		});
+	// 		this[1].css({
+	// 			'background': 'url(' + previewImage.src + ')' + ' center center no-repeat',
+	// 			'position': 'absolute',
+	// 			'-webkit-transform': 'scaleX(-1)',
+	// 			'background-size': 'cover'
+	// 		});
+	// 	};
+	// });
+
+	// SETTING THE NATIVE WIDTH OF EACH PREVIEW IMAGE
+	$('img.prevImg').each( function (i) {
 		var previewImage = new Image();
 		previewImage.src = $(this).attr('src');
-		previewImage.onload = function () {
+		console.log(this);
+
+		previewImage.onload = function (e) {
+
+			var matchWidth = this.parent().width();
+			var matchHeight = this.parent().height();
+
+			console.log(matchWidth + ' ' + matchHeight );
+			// if (matchWidth < e.)
+			var ratio = 1-(e.srcElement.width-matchWidth)/e.srcElement.width;
+			var newHeight = e.srcElement.height*ratio;
+
 			this.css({
-				'background': 'url(' + previewImage.src + ')' + ' center center no-repeat',
-				'background-size': 'cover'
+				'width': matchWidth,
+				'height': newHeight,
 			});
-		}.bind(background);
+		}.bind($(this));
 	});
 	
 
