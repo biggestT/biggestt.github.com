aa272595371c150c5622df33c251735f40880fef
Tor <tornilssonohrn@gmail.com>
2013-09-06 10:24:44 +0200

    circular pictures and no menu, just frontpage

--- _posts/2013-08-10-the-porters-and-the-mountain.md
+++ _posts/2013-08-10-the-porters-and-the-mountain.md
@@ -8,4 +8,8 @@ tags: [INUG, engingeers without borders]
 ---
 {% include JB/setup %}
 
-youtubelink etc.
+
+<p>
+	<object width="600" height="338"><param name="movie" value="//www.youtube.com/v/wfFo7AIe8e0?version=3&amp;hl=en_US&amp;rel=0"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="//www.youtube.com/v/wfFo7AIe8e0?version=3&amp;hl=en_US&amp;rel=0" type="application/x-shockwave-flash" width="600" height="338" allowscriptaccess="always" allowfullscreen="true"></embed></object>
+</p>
+Recorded in Tanzania during the summer of 2010
--- assets/themes/torManifest/js/torManifest.js
+++ assets/themes/torManifest/js/torManifest.js
@@ -44,62 +44,62 @@ $(function () {
 	// GLOCAL VARIABLES
 	// ---------------
 
-	var previewImages = [];
+	// var previewImages = [];
 
-	var constrainingDivId = '.container-narrow';
-	var matchWidth = $(constrainingDivId).width();
-	var matchHeight = $(constrainingDivId).height();
+	// var constrainingDivId = '.container-narrow';
+	// var matchWidth = $(constrainingDivId).width();
+	// var matchHeight = $(constrainingDivId).height();
 
-	// GATHER ALL OF THE PREVIEW IMAGES
-	// ----------------------------------
+	// // GATHER ALL OF THE PREVIEW IMAGES
+	// // ----------------------------------
 
-	var initalizePreviewImages = function () {
-		$('img.prevImg').each( function (i) {
-			previewImages[i] = new Image();
-			previewImages[i].src = $(this).attr('src');
-			previewImages[i].owner = $(this);
-			previewImages[i].onload = firstTimeCrop;
-		});
-	}
+	// var initalizePreviewImages = function () {
+	// 	$('img.prevImg').each( function (i) {
+	// 		previewImages[i] = new Image();
+	// 		previewImages[i].src = $(this).attr('src');
+	// 		previewImages[i].owner = $(this);
+	// 		previewImages[i].onload = firstTimeCrop;
+	// 	});
+	// }
 
 	
-	// CROPPING OF THE PREVIEW IMAGES
-	// ----------------------------------
+	// // CROPPING OF THE PREVIEW IMAGES
+	// // ----------------------------------
 	
-	var cropImage = function (img) {
-		var w = img.width;
-		var ratio = 1 - (img.width - matchWidth)/img.width;
-		var newHeight = img.height * ratio;
+	// var cropImage = function (img) {
+	// 	var w = img.width;
+	// 	var ratio = 1 - (img.width - matchWidth)/img.width;
+	// 	var newHeight = img.height * ratio;
 
-		img.owner.css({
-			'width': matchWidth,
-			'height': newHeight,
-		});
-		console.log(img);
-	}
+	// 	img.owner.css({
+	// 		'width': matchWidth,
+	// 		'height': newHeight,
+	// 	});
+	// 	console.log(img);
+	// }
 
-	var firstTimeCrop = function (e) {
-		cropImage(e.srcElement);
-	}
+	// var firstTimeCrop = function (e) {
+	// 	cropImage(e.srcElement);
+	// }
 	
-	var reCropAllPreviewImages = function () {
-		// update proportions to match
-		matchWidth = $(constrainingDivId).width();
-		matchHeight = $(constrainingDivId).height();
+	// var reCropAllPreviewImages = function () {
+	// 	// update proportions to match
+	// 	matchWidth = $(constrainingDivId).width();
+	// 	matchHeight = $(constrainingDivId).height();
 
-		for (i in previewImages) {
-			cropImage(previewImages[i]);
-		}
-	}
+	// 	for (i in previewImages) {
+	// 		cropImage(previewImages[i]);
+	// 	}
+	// }
 
-	// redo croppin if window size changes
-	// -------------------------------
-	window.onresize = reCropAllPreviewImages;
+	// // redo croppin if window size changes
+	// // -------------------------------
+	// window.onresize = reCropAllPreviewImages;
 
 
-	// Start doing the initial cropping!
-	// --------------------------
-	initalizePreviewImages();
+	// // Start doing the initial cropping!
+	// // --------------------------
+	// initalizePreviewImages();
 
 
 
